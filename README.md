6e624d88f190646e848cb3bdde5827a58cd035fe
Harry Ellerm <harryellerm@gmail.com>
2020-04-23 17:02:15 +1200

    Refactored and provided more detail in comments

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # NZ-Covid-19
 
 This script downloads data provided by the Ministry of Health related to COVID-19 in New Zealand and augments
-it with geo-location data. It loads then loads this data into dataframes which are then imported into Power BI
+it with geo-location data. It loads then loads this data into data frames which are then imported into Power BI
 as a way to hack up a quick dashboard.
 
 This mini-project was used as a way to experiment with importing data into Power-BI via a python script. It is not
@@ -10,7 +10,7 @@ intended to be used as an official source of information related to COVID-19.
 
 ### Initial Dashboard Mock Up
 
-![Dashboard Example](https://github.com/HarrisonEllerm/NZ-Covid-19/blob/master/Dashboard_Ex.PNG?raw=True)
+![Dashboard Example](https://github.com/HarrisonEllerm/NZ-Covid-19/blob/master/Dashboard_Ex_2.PNG?raw=True)
 
 Ministry of Health Link:
 
--- moh_scraper_v2.py
+++ moh_scraper_v2.py
@@ -17,10 +17,10 @@ loc_file_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Documents
 
 # Base data url for download link
 base_data_url = 'https://www.health.govt.nz'
-# URL used to grab key summary stats
+# URL used to grab key summary stats from summary table
 summary_stats_url = 'https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/' \
                     'covid-19-current-situation/covid-19-current-cases'
-# Current cases url where link exists
+# Current cases url where download link exists for data-set
 current_cases_url = 'https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19' \
                     '-current-situation/covid-19-current-cases/covid-19-current-cases-details'
 
@@ -31,9 +31,13 @@ geo_locator = Nominatim(user_agent='moh_scraper', timeout=10)
 def setup_data_source_path():
     """
     Sets up the data source path (path to download link).
-    Scrapes web page to find all links then looks for text
-    matching link description 'Download confirmed and probable
-    case data' on the MOH web page.
+
+    Scrapes web page to find all links then looks for text  matching link description
+    'Download confirmed and probable case data' on the MOH web page.
+
+    If the web page isn't live (or the link has changed) an HTTP status code will be returned
+    other than 200 'OK'. In this instance the script will exit (by design) as the link will
+    need to be updated.
 
     :return: download URL for MOH COVID-19 data file
     """
@@ -61,6 +65,21 @@ def setup_data_source_path():
 
 
 def get_key_stats():
+    """
+    Pulls key stats from the MOH web-page (Summary Table)
+
+    If the web page isn't live (or the link has changed) an HTTP status code will be returned
+    other than 200 'OK'. In this instance the script will exit (by design) as the link will
+    need to be updated.
+
+    :return: a data-frame containing stats for:
+       - Number of cases currently in hospital
+       - Number of recovered cases
+       - Number of deaths
+       - Increase in confirmed cases within 24 hours
+       - Increase in probable cases within 24 hours
+
+    """
     test_request = requests.get(summary_stats_url)
     # If web page is live search for link to download data
     if test_request.status_code == 200:
@@ -71,10 +90,26 @@ def get_key_stats():
         raw_summary_stats_df = pd.read_html(str(tables[0]))[0]
         raw_summary_stats_df.columns = ['Statistic', 'Total to date', 'New in last 24 hours']
         # Transpose
-        return raw_summary_stats_df.set_index('Statistic').T
+        raw_summary_stats_df = raw_summary_stats_df.set_index('Statistic').T
+        # Perform a few transformations to get data into format I want it in
+        summary_stats_df = raw_summary_stats_df.loc[['Total to date'], ['Number of cases currently in hospital',
+                                                                        'Number of recovered cases',
+                                                                        'Number of deaths']]
+        new_in_last_24_hours = raw_summary_stats_df.loc[
+            ['New in last 24 hours'], ['Number of confirmed cases in New Zealand',
+                                       'Number of probable cases']]
+        summary_stats_df['Increase in confirmed cases within 24 hours'] = new_in_last_24_hours.at[
+            'New in last 24 hours',
+            'Number of confirmed '
+            'cases in New Zealand']
+        summary_stats_df['Increase in probable cases within 24 hours'] = new_in_last_24_hours.at[
+            'New in last 24 hours',
+            'Number of probable cases']
+        return summary_stats_df
+
     else:
-        print(f'>> Could not find web-page, check that the link {summary_stats_url} is still valid... '
-              f'exiting')
+        print(f'>> Could not find web-page, check that the link {summary_stats_url} is still valid... ')
+        print(f'>> exiting...')
         exit(1)
 
 
@@ -88,7 +123,7 @@ def get_geo_loc(location_name, nz):
     below (see script output to find the location name).
 
     :param location_name: the name of the location i.e. 'Auckland'
-    :param nz: True if the location is in NZ (helps the geo-location of places within New Zealand) or False if not
+    :param nz: True if the location is in NZ or False if not
     :return: a dictionary containing the location name, latitude & longitude
     """
     # Custom mapping rules - due to geo-locator not being able to pick up some DHB's and locations
@@ -109,6 +144,9 @@ def get_geo_loc(location_name, nz):
         location_name = 'New Zealand'
 
     if nz:
+        # Have found that appending 'NZ' to the location name improves location data accuracy
+        # This problem doesn't persist for international locations as they are recorded as just a
+        # country name rather than a region within NZ
         data = geo_locator.geocode(f'{location_name}, NZ')
     else:
         data = geo_locator.geocode(f'{location_name}')
@@ -125,21 +163,19 @@ def download_file(from_url, to_folder):
     """
     Downloads a file and writes it to an output file within the base folder.
 
-    If the latest data has already been downloaded the script exits.
-
     :param from_url: the url to download the file from
     :param to_folder: the folder to write the output file to
     :return: the path to the file created
     """
     file_name_from_url = os.path.basename(from_url)
-    file_loc = to_folder + '\\' + file_name_from_url
+    file_loc = os.path.join(to_folder, file_name_from_url)
     new_file = open(file_loc, 'wb')
     response = get(from_url)
     new_file.write(response.content)
     return file_loc
 
 
-# Sets up the location fields within the output data frame
+# Sets up the location fields within the output data-frame
 # Queries a json file that holds geo-location data for previously
 # queried locations in order to reduce load on Nominatim API
 def setup_location_fields(df):
@@ -157,8 +193,8 @@ def setup_location_fields(df):
     Arrived_From_Latitude: The latitude of the country where this case arrived from, if any
     Arrived_From_Longitude: The longitude of the country where this case arrived from, if any
 
-    :param df: the DataFrame to which the four location fields above are being added
-    :return: the modified DataFrame
+    :param df: the data-frame to which the four location fields above are being added
+    :return: the modified data-frame
     """
 
     with open(loc_file_path, 'r') as json_file:
@@ -211,71 +247,67 @@ def setup_location_fields(df):
                          Arrived_From_Longitude=arrived_from_long_list)
 
 
+def setup_case_data(path_to_wb, sheet_name, confirmed_or_probable_identifier):
+    """
+    Reads in data from the downloaded workbook and cleans it up
+
+    :param path_to_wb: Path to the excel workbook being read in
+    :param sheet_name: Name of the sheet being read in from the workbook
+    :param confirmed_or_probable_identifier: 1 if the case is confirmed, 0 if the case is probable
+    :return: the case data data-frame
+    """
+    # Read in case data
+    df = pd.read_excel(path_to_wb, sheet_name=sheet_name, engine='openpyxl')
+    # Set columns and get rid of spaces above tabular data
+    df = pd.DataFrame(df.values[3:], columns=df.iloc[2])
+    # Clean up some rubbish columns
+    df = df.dropna(axis=1, how='all')
+    # Makes it easier to test if they travelled to a country prior to contracting
+    df = df.fillna('')
+    # Setup location data
+    df = setup_location_fields(df)
+    # Setup confirmed/probable identifier
+    df['Confirmed_or_Probable'] = confirmed_or_probable_identifier
+    return df
+
+
+# Note to self - do not include this in external main() as Power-BI cannot run it unless it is purely a script
+# Power-BI will detect the data-frames created by this script (cases_df & summary_df) and provide the option to
+# import them after the script has finished running.
 if __name__ == '__main__':
     print('>>> Script started')
+    # Get link to COVID-19 data file and download it if it exists
     data_source_path = setup_data_source_path()
     path_to_downloaded_file = download_file(from_url=data_source_path, to_folder=base_folder)
-    pd.options.display.width = 0
 
-    print(">> Building out confirmed cases...")
-    # Read in current cases
-    current_cases_raw_df = pd.read_excel(path_to_downloaded_file, sheet_name='Confirmed', engine='openpyxl')
-    current_cases_df = pd.DataFrame(current_cases_raw_df.values[3:], columns=current_cases_raw_df.iloc[2])
-    # Clean up some rubbish columns
-    current_cases_df = current_cases_df.dropna(axis=1, how='all')
-    # Makes it easier to test if they travelled to a country prior to contracting
-    current_cases_df = current_cases_df.fillna('')
-    # Setup location data
-    current_cases_df = setup_location_fields(current_cases_df)
-    # Setup confirmed/probable identifier
-    current_cases_df['Confirmed_or_Probable'] = 1
+    print(">> Building out case data...")
+    current_cases_df = setup_case_data(path_to_downloaded_file, 'Confirmed', 1)
+    probable_cases_df = setup_case_data(path_to_downloaded_file, 'Probable', 0)
 
-    print(">> Building out probable cases...")
-    # Read in probable cases
-    probable_cases_raw_df = pd.read_excel(path_to_downloaded_file, sheet_name='Probable', engine='openpyxl')
-    probable_cases_df = pd.DataFrame(probable_cases_raw_df.values[3:], columns=probable_cases_raw_df.iloc[2])
-    probable_cases_df = probable_cases_df.dropna(axis=1, how='all')
-    probable_cases_df = probable_cases_df.fillna('')
-    probable_cases_df = setup_location_fields(probable_cases_df)
-    # Setup confirmed/probable identifier
-    probable_cases_df['Confirmed_or_Probable'] = 0
-
-    # Merge into all cases
+    # --------------------------------------- Merge into all cases --------------------------------------- #
+    # It is easier to merge the two sources (current_cases and probable_cases) together and setup an
+    # identifier for the case being probable or confirmed because of the way Power-BI works when linking tables
+    # together via relationships. If, for example, you wanted to plot confirmed and probable cases over time (as I do)
+    # you would need to link these tables together using a date table (as there isn't always a confirmed/probable
+    # case that occurred on the same day. This is fine, but then when you also want to link the DHB fields together
+    # by setting up a relationship between the current cases table and probable cases table Power-BI wont let you
+    # as there is already a link between the tables (via the date fields linking to the date table).
+    # There is ways you can 'activate' certain relationships but this solution is generally easier here IMO.
+    # ---------------------------------------------------------------------------------------------------- #
     cases_df = current_cases_df.append(probable_cases_df, ignore_index=True)
 
+    # Grab summary stats
     print(">> Building out summary stats...")
-    # Grab summary stats should probably extract this logic out elsewhere
-    summary_stats_raw_df = get_key_stats()
+    summary_df = get_key_stats()
 
-    # Figure out a better way to do this - table headers keep changing
-    # Maybe use integer indexing instead
-
-    summary_stats_df = summary_stats_raw_df.loc[['Total to date'], ['Number of cases currently in hospital',
-                                                                    'Number of recovered cases', 'Number of deaths']]
-
-    new_in_last_24_hours = summary_stats_raw_df.loc[
-        ['New in last 24 hours'], ['Number of confirmed cases in New Zealand',
-                                   'Number of probable cases']]
-
-    summary_stats_df['Increase in confirmed cases within 24 hours'] = new_in_last_24_hours.at['New in last 24 hours',
-                                                                                              'Number of confirmed '
-                                                                                              'cases in New Zealand']
-    summary_stats_df['Increase in probable cases within 24 hours'] = new_in_last_24_hours.at['New in last 24 hours',
-                                                                                             'Number of probable cases']
-
-    # Get rid of raw data frames to avoid accidental import into Power-BI
+    # Get rid original data-frames (as we merged them into one above) to avoid accidental import into Power-BI
     del current_cases_df
     del probable_cases_df
-    del current_cases_raw_df
-    del probable_cases_raw_df
-    del summary_stats_raw_df
-    del new_in_last_24_hours
 
     # Export for COP
     print('>> Exporting for COP')
-    with pd.ExcelWriter(
-            f'{base_folder}\\COP_Export\\Covid_19_Data_For_Cop.xlsx') as writer:
+    with pd.ExcelWriter(f'{base_folder}\\COP_Export\\Covid_19_Data_For_Cop.xlsx') as writer:
         cases_df.to_excel(excel_writer=writer, sheet_name='Confirmed and Probable Cases', index=False)
-        summary_stats_df.to_excel(excel_writer=writer, sheet_name='Summary Stats', index=False)
+        summary_df.to_excel(excel_writer=writer, sheet_name='Summary Stats', index=False)
     print('>>> Ended')
 
